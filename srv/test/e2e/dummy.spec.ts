724073de2398fb06bdfbcfc3173aba051fd5c4a2
Siarhei Murkou <siarhei.murkou@sap.com>
2025-01-30 18:37:06 +0300

    [tempo]: Test out `deep delete`

--- srv/test/e2e/dummy.spec.ts
+++ srv/test/e2e/dummy.spec.ts
@@ -1,9 +1,15 @@
+import cds, { Service } from "@sap/cds";
+import { cds_runtime } from "../../src/cds";
 import { initializeMockCdsServer } from "./utils/init.mock.cds.server";
 
 describe("Dummy Service", () => {
     const { instance, start } = initializeMockCdsServer();
+    let db: Service;
 
-    beforeAll(() => start());
+    beforeAll(async () => {
+        await start();
+        db = await cds.connect.to("db");
+    });
 
     const shared_ID = "0000000001";
     const sub_ID = "0000000002";
@@ -29,6 +35,11 @@ describe("Dummy Service", () => {
         expect(response.status).toBe(201);
     });
 
+    it("should remove non-existing shared top object", async () => {
+        const where = { shared_ID, top_ID: "0000000000" };
+        await db.run(DELETE.from(cds_runtime.srv_dummy().SharedTopObjects).where(where));
+    });
+
     it("should get shared sub object", async () => {
         const response = await instance.get(`/dummy-service/SharedSubObjects`);
         expect(response.status).toBe(200);
@@ -41,22 +52,24 @@ describe("Dummy Service", () => {
     // ... and it must remove it together with shared sub object
     it("should remove shared top object", async () => {
         const response = await instance.delete(
-            `/dummy-service/SharedTopObjects(shared_ID='${shared_ID}',top_ID='${top_ID}')`,
+            // `/dummy-service/SharedTopObjects(shared_ID='${shared_ID}',top_ID='${top_ID}')`,
+            /** trying out the `deep` DELETE through the parent CDS projection */
+            `/dummy-service/SharedObjects/${shared_ID}/top(shared_ID='${shared_ID}',top_ID='${top_ID}')`,
         );
         expect(response.status).toBe(204);
     });
 
-    it("should should get neither shared sub nor shared top objects", async () => {
-        {
-            const response = await instance.get("/dummy-service/SharedTopObjects");
-            expect(response.status).toBe(200);
-            expect(response.data).toStrictEqual(expect.objectContaining({ value: [] }));
-        }
+    it("should get neither shared sub nor shared top objects", async () => {
+        const responses = await Promise.all([
+            instance.get("/dummy-service/SharedTopObjects"),
+            instance.get("/dummy-service/SharedSubObjects"),
+        ]);
 
-        {
-            const response = await instance.get("/dummy-service/SharedSubObjects");
-            expect(response.status).toBe(200);
-            expect(response.data).toStrictEqual(expect.objectContaining({ value: [] }));
-        }
+        for (const response of responses) expect(response.status).toBe(200);
+
+        expect(responses.map(({ data }) => data)).toStrictEqual([
+            expect.objectContaining({ value: [] }),
+            expect.objectContaining({ value: [] }),
+        ]);
     });
 });
