f7c58df7c6eaf818298dfeec9a55b3e5ccd66354
Siarhei Murkou <siarhei.murkou@sap.com>
2025-12-03 15:48:38 +0300

    Make `CDS deep UPDATE` fail with `max packet size exceeded`
    Note:
     It's only reproducible with HDB connection — It never fails on SQLite

--- db/dummy.entities.cds
+++ db/dummy.entities.cds
@@ -1,6 +1,24 @@
 namespace sap.cap.issues;
 
 entity Dummies {
-    name        : String not null;
-    description : String;
+    key ID          : UUID;
+        name        : String not null;
+        description : String;
+
+        sets        : Composition of many DummySets
+                          on sets.dummy = $self;
+}
+
+@assert.unique: {UK: [
+    dummy,
+    prop_001,
+    prop_002
+]}
+entity DummySets {
+    key ID                   : UUID;
+
+        prop_001             : UUID not null;
+        prop_002             : UUID not null;
+
+        @assert.target dummy : Association to one Dummies not null;
 }
--- srv/test/e2e/dummy.spec.ts
+++ srv/test/e2e/dummy.spec.ts
@@ -1,10 +1,25 @@
+import { randomUUID } from "node:crypto";
 import cds, { Service } from "@sap/cds";
-import { cds_db_dummy, cds_runtime } from "../../src/cds";
+import { cds_runtime } from "../../src/cds";
 import { initializeMockCdsServer } from "./utils/init.mock.cds.server";
 
 describe("Dummy Service", () => {
     const { instance, start } = initializeMockCdsServer();
     let db: Service;
+
+    const dummy_ID = randomUUID();
+    const prop_002 = randomUUID();
+
+    /**
+     * this size works fine for SQLite
+     *  but breaks queries to HANA with error "maximum packet size exceeded"
+     */
+    const SIZE = 3000;
+
+    const sets = [...Array(SIZE)].map(() => ({
+        prop_001: randomUUID(),
+        prop_002,
+    }));
 
     beforeAll(async () => {
         await start();
@@ -19,11 +34,34 @@ describe("Dummy Service", () => {
         expect(body.value).toStrictEqual(expect.any(Number));
     });
 
-    it("should get 0 dummies", async () => {
-        const dummies: cds_db_dummy.Dummy[] = await db.run(
-            SELECT.from(cds_runtime.db_dummy().Dummies),
+    it("should initialize empty dummy", async () => {
+        await db.run(
+            INSERT.into(cds_runtime.db_dummy().Dummies).entries({
+                ID: dummy_ID,
+                name: "tempo",
+                sets: [],
+            }),
         );
+    });
 
-        expect(dummies).toStrictEqual([]);
+    it("should populate dummy sets", async () => {
+        await db.run(
+            UPDATE.entity(cds_runtime.db_dummy().Dummies).byKey(dummy_ID).set({
+                sets: sets,
+            }),
+        );
+    });
+
+    it("should update dummy sets", async () => {
+        await db.run(
+            UPDATE.entity(cds_runtime.db_dummy().Dummies)
+                .byKey(dummy_ID)
+                .set({
+                    sets: sets.map((s, i) => ({
+                        prop_001: i > SIZE - 10 ? randomUUID() : s.prop_001,
+                        prop_002,
+                    })),
+                }),
+        );
     });
 });
